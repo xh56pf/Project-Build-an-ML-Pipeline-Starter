2cdba73c8f2a9f3b92b9f38fd9311ee1159f0022
xh56ph <xin.he56@gmail.com>
2026-04-03 23:20:34 +0000

    My code

--- main.py
+++ main.py
@@ -53,19 +53,57 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-            pass
+            _ = mlflow.run( 
+                "src/basic_cleaning",
+                "main",
+                env_manager="conda",
+                parameters={
+                    "input_artifact":"sample.csv:latest",
+                    "output_artifact":"cleaned_sample.csv",
+                    "output_type":"clean_sample",
+                    "output_description":"Data with basic cleaning applied",
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"],
+
+                },
+
+            )
+            
 
         if "data_check" in active_steps:
             ##################
             # Implement here #
             ##################
-            pass
+            _ = mlflow.run( 
+                "src/data_check",
+                "main",
+                env_manager="conda",
+                parameters={
+                    "csv":"cleaned_sample.csv:latest",
+                    "ref":"cleaned_sample.csv:reference",
+                    "kl_threshold":config["data_check"]['kl_threshold'],
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"],
+
+
+                },
+            )
 
         if "data_split" in active_steps:
             ##################
             # Implement here #
             ##################
-            pass
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                'main',
+                env_manager="conda",
+                parameters = {
+                    "input":"cleaned_sample.csv:latest",
+                    "test_size":config["modeling"]["test_size"],
+                    "random_seed":config["modeling"]["random_seed"],
+                    "stratify_by":config["modeling"]["stratify_by"],
+                },
+            )
 
         if "train_random_forest" in active_steps:
 
@@ -81,15 +119,38 @@ def go(config: DictConfig):
             # Implement here #
             ##################
 
-            pass
+            _ = mlflow.run( 
+                "src/train_random_forest",
+                "main",
+                env_manager="conda",
+                parameters={
+                    "trainval_artifact":"trainval_data.csv:latest",
+                    "val_size":config["modeling"]["val_size"],
+                    "random_seed":config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                    "rf_config": rf_config,
+                    "max_tfidf_features":config["modeling"]["max_tfidf_features"],
+                    "output_artifact":"random_forest_export",
+
+
+                },
+            )
 
         if "test_regression_model" in active_steps:
 
             ##################
             # Implement here #
             ##################
-
-            pass
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                'main',
+                env_manager="conda",
+                parameters = {
+                    "mlflow_model":"random_forest_export:prod",
+                    "test_dataset":"test_data.csv:latest"
+                }
+            )
+            
 
 
 if __name__ == "__main__":
--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -75,6 +75,8 @@ def go(args):
     # Fit the pipeline sk_pipe by calling the .fit method on X_train and y_train
     # YOUR CODE HERE
     ######################################
+    sk_pipe.fit(X_train[processed_features], y_train)
+
 
     # Compute r2 and MAE
     logger.info("Scoring")
@@ -97,7 +99,9 @@ def go(args):
     # HINT: use mlflow.sklearn.save_model
     mlflow.sklearn.save_model(
         # YOUR CODE HERE
-        input_example = X_train.iloc[:5]
+        sk_pipe,
+        "random_forest_dir",
+        input_example = X_train.iloc[:5],
     )
     ######################################
 
@@ -121,7 +125,7 @@ def go(args):
     # Now save the variable mae under the key "mae".
     # YOUR CODE HERE
     ######################################
-
+    run.summary['mae'] = mae
     # Upload to W&B the feture importance visualization
     run.log(
         {
@@ -163,6 +167,7 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
     # 2 - A OneHotEncoder() step to encode the variable
     non_ordinal_categorical_preproc = make_pipeline(
         # YOUR CODE HERE
+        SimpleImputer(strategy="most_frequent"),OneHotEncoder()
     )
     ######################################
 
@@ -226,6 +231,8 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
     sk_pipe = Pipeline(
         steps =[
         # YOUR CODE HERE
+            ("preprocessor", preprocessor),
+            ("random_forest", random_forest),
         ]
     )
 
